1798987c29d57c4b0a5eff1a30fa0ffd4275affa
Quang Tien <phan.quang.tien@framgia.com>
2017-09-03 09:41:42 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App__header">
+          <h2>What's Next</h2>
+        </div>
+        <div className="App__content">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
